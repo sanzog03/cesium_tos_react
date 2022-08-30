9b99984789aa1ba11119ce0c50baac25ae0f6b69
sanjog <sanzog03@gmail.com>
2022-08-30 09:32:38 -0500

    env added

--- src/modules/cesium/index.jsx
+++ src/modules/cesium/index.jsx
@@ -21,7 +21,7 @@ class FCXViewer extends Component {
     }
 
     startDrawing() {
-        Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhYTg3MTQxNS01ZTIwLTQ4MmItYjA5NS1hYWM3MWQ0OTNkYTMiLCJpZCI6MTA1ODMzLCJpYXQiOjE2NjE0MzkwOTB9.IWdoSi1zjC7fl7Ncj0YVJgXMfjX3K-RmRcGtjp2xryo";
+        Cesium.Ion.defaultAccessToken = process.env.REACT_APP_CESIUM_DEFAULT_ACCESS_TOKEN;
         // Add viewer(Camera) with cesium world terrain
         const viewer = new Cesium.Viewer('cesiumContainer', {
             terrainProvider: Cesium.createWorldTerrain({
