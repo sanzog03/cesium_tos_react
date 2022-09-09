3ce595f812ca97852e5831480991e38b71ead3ae
Sanjog Thapa <sanzog03@gmail.com>
2022-09-09 15:49:05 -0500

    removed unnecessary code to change camera look at transform

--- src/modules/cesium/index.jsx
+++ src/modules/cesium/index.jsx
@@ -168,62 +168,41 @@ class FCXViewer extends Component {
             // Cesium.CzmlDataSource.load("./testData.czml")
             // Cesium.CzmlDataSource.load("https://fcx-czml.s3.amazonaws.com/flight_track/goesrplt_naver2_IWG1_20170322-0136")
             Cesium.CzmlDataSource.load(dataImpact)
-            .then((dataSource) => {
+            .then(async (dataSource) => {
                 viewer.dataSources.add(dataSource);
-
-                let p3Entity = dataSource.entities.getById("Flight Track");
                 const clock = viewer.clock;
+                const p3Entity = dataSource.entities.getById("Flight Track");
                 
-                ////////////////////////////////////////
                 // set the camera orientation and keep it far apart from the model.
 
-                // viewer.zoomTo(dataSource,  new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000));
-                // viewer.trackedEntity = p3Entity;
+                // a fly to is much better than zoom to.
+                viewer.zoomTo(dataSource,  new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000));
+                // await viewer.flyTo(p3Entity, {offset: new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000)})
+
                 // viewer.camera.Zoomout(10000000);
+                // viewer.camera.setView({ destination: p3Entity, orientation: new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000)})
 
-                //Set up chase camera
-                var matrix3Scratch = new Cesium.Matrix3();
-                var positionScratch = new Cesium.Cartesian3();
-                var orientationScratch = new Cesium.Quaternion();
-                function getModelMatrix(entity, time, result) {
-                    var position = Cesium.Property.getValueOrUndefined(entity.position, time, positionScratch);
-                    if (!Cesium.defined(position)) {
-                        return undefined;
-                    }
-                    var orientation = Cesium.Property.getValueOrUndefined(entity.orientation, time, orientationScratch);
-                    if (!Cesium.defined(orientation)) {
-                        result = Cesium.Transforms.eastNorthUpToFixedFrame(position, undefined, result);
-                    } else {
-                        result = Cesium.Matrix4.fromRotationTranslation(Cesium.Matrix3.fromQuaternion(orientation, matrix3Scratch), position, result);
-                    }
-                    return result;
-                }
-
-                var scratch = new Cesium.Matrix4();
-                var camera =  viewer.scene.camera;
-                viewer.scene.preRender.addEventListener(function(){
-                    getModelMatrix(p3Entity, viewer.clock.currentTime, scratch);
-                    camera.lookAtTransform(scratch, new Cesium.Cartesian3(-100000, 20, 90));
-                });
-
-                ///////////////////////////////////////////////////////////
-                  
-                // change the model orientation
-                const heading = Cesium.Math.toRadians(270);
-                const pitch = Cesium.Math.toRadians(90);
-                const roll = Cesium.Math.toRadians(0);
+                viewer.trackedEntity = p3Entity;
 
                 clock.onTick.addEventListener(() => {
-                    const position = p3Entity.position.getValue(clock.currentTime);
-                    // the heading should change with respect to the position.
-                    // TODO: if possible get the heading roll and pitch from the czml data itself. But question is when to take which data.
-                    const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
-                    const orientation = Cesium.Transforms.headingPitchRollQuaternion(
-                        position,
-                        hpr
-                    );
-                    p3Entity.orientation = orientation;
+                    fixOrientation();
                 });
+
+                function fixOrientation() {
+                    // change the model orientation
+                    const heading = Cesium.Math.toRadians(270);
+                    const pitch = Cesium.Math.toRadians(90);
+                    const roll = Cesium.Math.toRadians(0);
+                    const position = p3Entity.position.getValue(clock.currentTime);
+                        // the heading should change with respect to the position.
+                        // TODO: if possible get the heading roll and pitch from the czml data itself. But question is when to take which data.
+                        const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+                        const orientation = Cesium.Transforms.headingPitchRollQuaternion(
+                            position,
+                            hpr
+                        );
+                    p3Entity.orientation = orientation;
+                }
             });
         }
     }
