795ba0ff9fd3f8163559eb08dd2ad460af151652
Sanjog Thapa <sanzog03@gmail.com>
2022-09-10 22:52:33 -0500

    seperated out code for czml viewer

--- src/modules/cesium/index.jsx
+++ src/modules/cesium/index.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import dataImpact from "./impactData.czml";
-import defaultViewer from "./viewers/default.js";
 import * as Cesium from 'cesium';
+// import dataImpact from "./impactData.czml";
+import defaultViewer from "./viewers/default.js";
+import CZMLPathViewer from "./viewers/czmlPathViewer.js";
 
 // The URL on your server where CesiumJS's static files are hosted.
 window.CESIUM_BASE_URL = '/';
@@ -15,7 +16,7 @@ class FCXViewer extends Component {
             currentViewer: null
         };
 
-        this.CZMLPathViewer = this.CZMLPathViewer.bind(this);
+        // this.CZMLPathViewer = this.CZMLPathViewer.bind(this);
         this.pointCloudViewer = this.pointCloudViewer.bind(this);
         this.handleSelectionChange = this.handleSelectionChange.bind(this);
         this.implementationHandler = this.implementationHandler.bind(this);
@@ -36,7 +37,7 @@ class FCXViewer extends Component {
         }
         switch(this.state.currentlyShowing) {
             case "czml":
-                this.CZMLPathViewer();
+                CZMLPathViewer(this.setCurrentViewer);
                 break;
             case "general":
                 defaultViewer(this.setCurrentViewer);
@@ -45,92 +46,92 @@ class FCXViewer extends Component {
                 this.pointCloudViewer();
                 break;
             default:
-                this.CZMLPathViewer();    
+                CZMLPathViewer(this.setCurrentViewer); 
         }
     }
 
-    CZMLPathViewer() {
+    // CZMLPathViewer() {
 
-        // var viewer = new Cesium.Viewer('cesiumContainer');
-        // var baseLayerPickerViewModel = viewer.baseLayerPicker.viewModel;
-        // baseLayerPickerViewModel.selectedImagery = baseLayerPickerViewModel.imageryProviderViewModels[0];
+    //     // var viewer = new Cesium.Viewer('cesiumContainer');
+    //     // var baseLayerPickerViewModel = viewer.baseLayerPicker.viewModel;
+    //     // baseLayerPickerViewModel.selectedImagery = baseLayerPickerViewModel.imageryProviderViewModels[0];
 
-        // var viewer = new Cesium.Viewer('cesiumContainer', {
-        //     imageryProvider : Cesium.createWorldImagery({
-        //         style : Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS
-        //     }),
-        //     baseLayerPicker : false
-        // });
+    //     // var viewer = new Cesium.Viewer('cesiumContainer', {
+    //     //     imageryProvider : Cesium.createWorldImagery({
+    //     //         style : Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS
+    //     //     }),
+    //     //     baseLayerPicker : false
+    //     // });
 
-        const viewer = new Cesium.Viewer("cesiumContainer", {
-            // terrainProvider: Cesium.createWorldTerrain(
-            //     {
-            //     requestWaterMask : true,
-            //     requestVertexNormals : true}
-            // ),
-            shouldAnimate: false,
-            useBrowserRecommendedResolution: true,
-            selectedImageryProviderViewModel: new Cesium.ProviderViewModel({
-                name: "Bing Maps Aerial with Labels",
-                iconUrl: Cesium.buildModuleUrl("Widgets/Images/ImageryProviders/bingAerialLabels.png"),
-                tooltip: "Bing Maps aerial imagery with labels, provided by Cesium ion",
-                category: "Cesium ion",
-                creationFunction: function () {
-                  return Cesium.createWorldImagery({
-                    style: Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS,
-                  })
-                },
-              })
-        });
+    //     const viewer = new Cesium.Viewer("cesiumContainer", {
+    //         // terrainProvider: Cesium.createWorldTerrain(
+    //         //     {
+    //         //     requestWaterMask : true,
+    //         //     requestVertexNormals : true}
+    //         // ),
+    //         shouldAnimate: false,
+    //         useBrowserRecommendedResolution: true,
+    //         selectedImageryProviderViewModel: new Cesium.ProviderViewModel({
+    //             name: "Bing Maps Aerial with Labels",
+    //             iconUrl: Cesium.buildModuleUrl("Widgets/Images/ImageryProviders/bingAerialLabels.png"),
+    //             tooltip: "Bing Maps aerial imagery with labels, provided by Cesium ion",
+    //             category: "Cesium ion",
+    //             creationFunction: function () {
+    //               return Cesium.createWorldImagery({
+    //                 style: Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS,
+    //               })
+    //             },
+    //           })
+    //     });
 
-        this.setState({currentViewer: viewer});
+    //     this.setState({currentViewer: viewer});
 
-        // viewer.extend(Cesium.viewerCesiumInspectorMixin);
+    //     // viewer.extend(Cesium.viewerCesiumInspectorMixin);
 
-        doStuffWithCZML()
+    //     doStuffWithCZML()
 
-        function doStuffWithCZML() {
-            // Cesium.CzmlDataSource.load("./testData.czml")
-            // Cesium.CzmlDataSource.load("https://fcx-czml.s3.amazonaws.com/flight_track/goesrplt_naver2_IWG1_20170322-0136")
-            Cesium.CzmlDataSource.load(dataImpact)
-            .then(async (dataSource) => {
-                viewer.dataSources.add(dataSource);
-                const clock = viewer.clock;
-                const p3Entity = dataSource.entities.getById("Flight Track");
+    //     function doStuffWithCZML() {
+    //         // Cesium.CzmlDataSource.load("./testData.czml")
+    //         // Cesium.CzmlDataSource.load("https://fcx-czml.s3.amazonaws.com/flight_track/goesrplt_naver2_IWG1_20170322-0136")
+    //         Cesium.CzmlDataSource.load(dataImpact)
+    //         .then(async (dataSource) => {
+    //             viewer.dataSources.add(dataSource);
+    //             const clock = viewer.clock;
+    //             const p3Entity = dataSource.entities.getById("Flight Track");
                 
-                // set the camera orientation and keep it far apart from the model.
+    //             // set the camera orientation and keep it far apart from the model.
 
-                // a fly to is much better than zoom to.
-                viewer.zoomTo(dataSource,  new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000));
-                // await viewer.flyTo(p3Entity, {offset: new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000)})
+    //             // a fly to is much better than zoom to.
+    //             viewer.zoomTo(dataSource,  new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000));
+    //             // await viewer.flyTo(p3Entity, {offset: new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000)})
 
-                // viewer.camera.Zoomout(10000000);
-                // viewer.camera.setView({ destination: p3Entity, orientation: new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000)})
+    //             // viewer.camera.Zoomout(10000000);
+    //             // viewer.camera.setView({ destination: p3Entity, orientation: new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-10), 40000)})
 
-                viewer.trackedEntity = p3Entity;
+    //             viewer.trackedEntity = p3Entity;
 
-                clock.onTick.addEventListener(() => {
-                    fixOrientation();
-                });
+    //             clock.onTick.addEventListener(() => {
+    //                 fixOrientation();
+    //             });
 
-                function fixOrientation() {
-                    // change the model orientation
-                    const heading = Cesium.Math.toRadians(270);
-                    const pitch = Cesium.Math.toRadians(90);
-                    const roll = Cesium.Math.toRadians(0);
-                    const position = p3Entity.position.getValue(clock.currentTime);
-                        // the heading should change with respect to the position.
-                        // TODO: if possible get the heading roll and pitch from the czml data itself. But question is when to take which data.
-                        const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
-                        const orientation = Cesium.Transforms.headingPitchRollQuaternion(
-                            position,
-                            hpr
-                        );
-                    p3Entity.orientation = orientation;
-                }
-            });
-        }
-    }
+    //             function fixOrientation() {
+    //                 // change the model orientation
+    //                 const heading = Cesium.Math.toRadians(270);
+    //                 const pitch = Cesium.Math.toRadians(90);
+    //                 const roll = Cesium.Math.toRadians(0);
+    //                 const position = p3Entity.position.getValue(clock.currentTime);
+    //                     // the heading should change with respect to the position.
+    //                     // TODO: if possible get the heading roll and pitch from the czml data itself. But question is when to take which data.
+    //                     const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
+    //                     const orientation = Cesium.Transforms.headingPitchRollQuaternion(
+    //                         position,
+    //                         hpr
+    //                     );
+    //                 p3Entity.orientation = orientation;
+    //             }
+    //         });
+    //     }
+    // }
 
     pointCloudViewer() {   
         Cesium.Ion.defaultAccessToken = process.env.REACT_APP_CESIUM_DEFAULT_ACCESS_TOKEN;
