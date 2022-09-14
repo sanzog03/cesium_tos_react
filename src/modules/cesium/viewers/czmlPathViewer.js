b43f133440231767297380a461c406afd397fa1e
sanjog <sanzog03@gmail.com>
2022-09-14 11:28:41 -0500

    orientation fix for p3 model. got the heading information from cesium callback property. changed the pitch and the rolling values to fix the orientation.

--- src/modules/cesium/viewers/czmlPathViewer.js
+++ src/modules/cesium/viewers/czmlPathViewer.js
@@ -1,4 +1,4 @@
-import { Viewer, createWorldImagery, IonWorldImageryStyle, createWorldTerrain, ProviderViewModel, buildModuleUrl, viewerCesiumInspectorMixin, CzmlDataSource, HeadingPitchRange, Math, HeadingPitchRoll, Transforms } from 'cesium';
+import { Viewer, createWorldImagery, IonWorldImageryStyle, createWorldTerrain, ProviderViewModel, buildModuleUrl, viewerCesiumInspectorMixin, CzmlDataSource, HeadingPitchRange, Math, HeadingPitchRoll, Transforms, CallbackProperty } from 'cesium';
 import impactData from "../datas/impactData.czml";
 
 export default function CZMLPathViewer(setCurrentViewer) {
@@ -15,11 +15,6 @@ export default function CZMLPathViewer(setCurrentViewer) {
     // });
 
     const viewer = new Viewer("cesiumContainer", {
-        // terrainProvider: createWorldTerrain(
-        //     {
-        //     requestWaterMask : true,
-        //     requestVertexNormals : true}
-        // ),
         shouldAnimate: false,
         useBrowserRecommendedResolution: true,
         selectedImageryProviderViewModel: new ProviderViewModel({
@@ -37,48 +32,43 @@ export default function CZMLPathViewer(setCurrentViewer) {
 
     setCurrentViewer(viewer);
 
-    // viewer.extend(viewerCesiumInspectorMixin);
-
     doStuffWithCZML()
 
     function doStuffWithCZML() {
-        // CzmlDataSource.load("./testData.czml")
-        // CzmlDataSource.load("https://fcx-czml.s3.amazonaws.com/flight_track/goesrplt_naver2_IWG1_20170322-0136")
         CzmlDataSource.load(impactData)
         .then(async (dataSource) => {
             viewer.dataSources.add(dataSource);
-            const clock = viewer.clock;
             const p3Entity = dataSource.entities.getById("Flight Track");
             
-            // set the camera orientation and keep it far apart from the model.
-
             // a fly to is much better than zoom to.
-            viewer.zoomTo(dataSource,  new HeadingPitchRange(0, Math.toRadians(-10), 40000));
             // await viewer.flyTo(p3Entity, {offset: new HeadingPitchRange(0, Math.toRadians(-10), 40000)})
 
+            viewer.zoomTo(dataSource,  new HeadingPitchRange(0, Math.toRadians(-10), 40000));
+
+            // camera zooming
+            // set the camera orientation and keep it far apart from the model.
             // viewer.camera.Zoomout(10000000);
             // viewer.camera.setView({ destination: p3Entity, orientation: new HeadingPitchRange(0, Math.toRadians(-10), 40000)})
 
+            // track entity
             viewer.trackedEntity = p3Entity;
 
-            clock.onTick.addEventListener(() => {
-                fixOrientation();
-            });
+            // fix orientation
+            p3Entity.orientation = new CallbackProperty((time, _result) => fixOrientation(p3Entity, time), false);
 
-            function fixOrientation() {
-                // change the model orientation
-                const heading = Math.toRadians(270);
-                const pitch = Math.toRadians(90);
-                const roll = Math.toRadians(0);
-                const position = p3Entity.position.getValue(clock.currentTime);
-                    // the heading should change with respect to the position.
-                    // TODO: if possible get the heading roll and pitch from the czml data itself. But question is when to take which data.
-                    const hpr = new HeadingPitchRoll(heading, pitch, roll);
-                    const orientation = Transforms.headingPitchRollQuaternion(
-                        position,
-                        hpr
-                    );
-                p3Entity.orientation = orientation;
+            function fixOrientation(entity, time) {
+                const position = entity.position.getValue(time);
+                let { heading, pitch, roll } = entity.properties.getValue(time);
+                // only the heading should change with respect to the position.
+                // fix the pitch and roll rotations
+                pitch = Math.toRadians(90);
+                roll = Math.toRadians(90);
+                const hpr = new HeadingPitchRoll(heading, pitch, roll);
+                const fixedOrientation = Transforms.headingPitchRollQuaternion(
+                    position,
+                    hpr
+                );
+                return fixedOrientation;
             }
         });
     }
