1c8d9e87a3704d48bdc2a114787a86d736e5460e
sanjog <sanzog03@gmail.com>
2022-09-21 11:48:47 -0500

    added styles to 3D tileset

--- src/modules/cesium/viewers/pointCloudViewer.js
+++ src/modules/cesium/viewers/pointCloudViewer.js
@@ -5,7 +5,7 @@ import GLMpoints from '../datas/CZMLS_FCX_05172017/3dTile/tileset_goesrpltcrs_ph
 import { extendCesium3DTileset } from 'temporal-3d-tile';
 const Temporal3DTileset = extendCesium3DTileset(Cesium);
 
-export default function pointCloudViewer(setCurrentViewer) {   
+export default function pointCloudViewer(setCurrentViewer) {
     const viewer = new Viewer("cesiumContainer", {
         terrainProvider: createWorldTerrain(),
         shouldAnimate: true,
@@ -19,13 +19,18 @@ export default function pointCloudViewer(setCurrentViewer) {
         // url: 'https://ghrc-fcx-viz-output.s3.us-west-2.amazonaws.com/fieldcampaign/goesrplt/2017-05-17/iss-lis_points/LISpoints.json'
         url: 'https://ghrc-fcx-viz-output.s3.us-west-2.amazonaws.com/fieldcampaign/goesrplt/2017-05-17/crs/tileset.json'
     });
-    // const clock = new Cesium.Clock({
-    //     startTime : Cesium.JulianDate.fromIso8601("2013-12-25"),
-    //     currentTime : Cesium.JulianDate.fromIso8601("2013-12-25"),
-    //     stopTime : Cesium.JulianDate.fromIso8601("2013-12-26"),
-    //     clockRange : Cesium.ClockRange.LOOP_STOP,
-    //     clockStep : Cesium.ClockStep.SYSTEM_CLOCK_MULTIPLIER
-    //  });
+
+    // tileset.pointCloudShading.attenuation = true;
+    tileset.style = new Cesium.Cesium3DTileStyle({
+        // color: 'mix(color("yellow"), color("red"), -1*${value})',
+        // color: { conditions: [
+        //                         ["${value} < -33", "rgb(0, 255, 0)"],
+        //                         ["true", "rgb(255+${value}, 255+${value}, 255+${value})"]
+        //                     ]
+        //         },
+        color: getColorExpression(),
+        pointSize: 2.0
+      });
 
      var currentTime = Cesium.JulianDate.fromIso8601("2017-05-17T02:23:33Z")
      var endTime = Cesium.JulianDate.fromIso8601("2017-05-18");
@@ -36,4 +41,20 @@ export default function pointCloudViewer(setCurrentViewer) {
 
     viewer.scene.primitives.add(tileset);
     viewer.zoomTo(tileset);
+    }
+
+    function getColorExpression() {
+        let reverse = true
+        let ascale = 4.346
+        let vmin = -10
+        let vmax = 30
+        let vrange = vmax - vmin
+        let hmin = 0.438
+        let hrange = 1
+
+        let revScale = ""
+        if (reverse) {
+            revScale = " * -1.0 + 1.0"
+        }
+        return `hsla((((clamp(\${value}, ${vmin}, ${vmax}) + ${vmin}) / ${vrange}) ${revScale}) * ${hrange} + ${hmin}, 1.0, 0.5, pow((\${value} - ${vmin})/${vrange}, ${ascale}))`
     }
