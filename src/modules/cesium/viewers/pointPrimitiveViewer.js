cf201f0a33eb263e910e3cbbd32d62c81a73ef3c
sanjog <sanzog03@gmail.com>
2022-09-21 13:34:22 -0500

    random color for each points, scale and pixel size varied in point primitive viewer

--- src/modules/cesium/viewers/pointPrimitiveViewer.js
+++ src/modules/cesium/viewers/pointPrimitiveViewer.js
@@ -1,4 +1,5 @@
-import { Viewer ,createWorldTerrain , Color, PointPrimitiveCollection, Cartesian3 } from 'cesium';
+import { Viewer ,createWorldTerrain , Color, PointPrimitiveCollection, Cartesian3,  NearFarScalar } from 'cesium';
+// import LISpoints from '../datas/CZMLS_FCX_05172017/points/GLMpoints.json';
 import LISpoints from '../datas/CZMLS_FCX_05172017/points/LISpoints.json';
 
 export default function pointPrimitiveViewer(setCurrentViewer) {
@@ -19,12 +20,33 @@ function basicPlotter (viewer, packet) {
     const points = viewer.scene.primitives.add(primitiveCollection);
 
     Lon.forEach((element, index) => {
+        let pw = 0.6, fct = 1/15;
+        let nFScalar = new NearFarScalar(1.e2, 2, 8.0e6, 0.5);
         points.add({
             // position : new Cartesian3(1.0, 2.0, 3.0),
             position: Cartesian3.fromDegrees(Lon[index], Lat[index]),
-            color : Color.RED
+            color : selectColor(),
+            pixel: Math.pow(Rad[index], pw) * fct,
+            scaleByDistance: nFScalar
             });
     });
+}
+
+function selectColor() {
+    let colors = {
+        yellow: new Color(1.0, 1.0, 0.4, 1),
+        cyan: new Color(0.68, 1.0, 0.55, .6),
+        orng: Color.ORANGE.brighten(0.5, new Color())
+    }
+    let rand = Math.floor(Math.random() * 3);
+    switch (rand) {
+        case 0:
+            return colors.yellow;
+        case 1:
+            return colors.cyan;
+        default:
+            return colors.orng;
+    }
 }
 
 function basicPlotterKTM (viewer) {
