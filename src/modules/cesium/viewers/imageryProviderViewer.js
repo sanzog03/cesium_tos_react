b33c7bc2143d9d4eb717ffc68ac68a5b454205ed
sanjog <sanzog03@gmail.com>
2023-03-07 11:48:05 -0600

    make image visible according to availability time.

--- src/modules/cesium/viewers/imageryProviderViewer.js
+++ src/modules/cesium/viewers/imageryProviderViewer.js
@@ -44,10 +44,16 @@ function imageViewerCZML(viewer) {
       id: "document",
       name: "CZML Geometries: Rectangle",
       version: "1.0",
+      clock: {
+        interval: "2015-09-22T22:28:00Z/2015-09-22T23:58:00Z",
+        currentTime: "2015-09-22T22:28:00Z",
+        multiplier: 20,
+      },
     },
     {
-      id: "textureRectangle",
+      id: "textureRectangle1",
       name: "rectangle with image, above surface",
+      availability: "2015-09-22T22:29:00Z/2015-09-22T22:38:00Z",
       rectangle: {
         coordinates: {
           wsenDegrees: [-123.197, 48.735, -121.812, 49.653],
@@ -57,6 +63,26 @@ function imageViewerCZML(viewer) {
         material: {
           image: {
             image: { uri: "https://ghrc-fcx-field-campaigns-szg.s3.amazonaws.com/Olympex/instrument-raw-data/nexrad/katx/2015-09-22/olympex_Level2_KATX_20150922_2229_ELEV_01.png" },
+            color: {
+              rgba: [255, 255, 255, 128],
+            },
+          },
+        },
+      },
+    },
+    {
+      id: "textureRectangle2",
+      name: "rectangle with image, above surface",
+      availability: "2015-09-22T22:38:00Z/2015-09-22T22:48:00Z",
+      rectangle: {
+        coordinates: {
+          wsenDegrees: [-123.197, 48.735, -121.812, 49.653],
+        },
+        height: 0,
+        fill: true,
+        material: {
+          image: {
+            image: { uri: "https://ghrc-fcx-field-campaigns-szg.s3.amazonaws.com/Olympex/instrument-raw-data/nexrad/katx/2015-09-22/olympex_Level2_KATX_20150922_2238_ELEV_01.png", },
             color: {
               rgba: [255, 255, 255, 128],
             },
